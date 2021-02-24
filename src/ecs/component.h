20c5c5bc74658923bb3e49285268eed8f16e30aa
Vladislav Oleshko <vladislav.oleshko@gmail.com>
2021-02-24 19:26:16 +0300

    Add Component and System classes

---
+++ src/ecs/component.h
@@ -0,0 +1,14 @@
+#ifndef SRC_ECS_COMPONENT_H_
+#define SRC_ECS_COMPONENT_H_
+
+namespace ecs {
+
+/**
+ * Common base for all pure data structs
+ */
+struct Component {
+};
+
+}  // namespace ecs
+
+#endif  // SRC_ECS_COMPONENT_H_
---
+++ src/ecs/system.h
@@ -0,0 +1,20 @@
+#ifndef SRC_ECS_SYSTEM_H_
+#define SRC_ECS_SYSTEM_H_
+
+namespace ecs {
+
+class World;
+
+class System {
+ public:
+  virtual ~System() = default;
+  /**
+   * Called on each tick
+   * @param world
+   */
+  virtual void run(World* world) = 0;
+};
+
+}  // namespace ecs
+
+#endif  // SRC_ECS_SYSTEM_H_
