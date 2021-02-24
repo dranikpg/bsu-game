a9d4a0e86392dfc9144f0138f37fddd84b97a79e
Vladislav Oleshko <vladislav.oleshko@gmail.com>
2021-02-24 20:00:09 +0300

    Test ECS

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,52 @@
 #include <QApplication>
 #include <QLabel>
+#include <QListWidget>
+
+#include "ecs/ecs.h"
+
+struct Message : public ecs::Component {
+  QString message;
+  Message(const QString& message) : message(message) {}
+};
+
+struct Visible : public ecs::Component {
+};
+
+/**
+ * Should add exactly two items: "First visible" and "Second visible"
+ */
+void TestEcs(QListWidget* list) {
+  ecs::World world;
+
+  world.CreateEntity()
+      .Create<Message>("First visible")
+      .Create<Visible>();
+
+  world.CreateEntity()
+      .Create<Message>("Second invisible");
+
+  ecs::Entity& second_visible = world.CreateEntity()
+      .Create<Message>("Second visible")
+      .Create<Visible>();
+
+  world.CreateEntity()
+      .Create<Message>("Second visible")
+      .Create<Visible>();
+
+  world.EraseEntity(second_visible);
+
+  world.Run();
+
+  for (auto[message, _] : world.Scan<Message, Visible>()) {
+    list->addItem(message.message);
+  }
+}
 
 int main(int argc, char* argv[]) {
   QApplication a(argc, argv);
-  QLabel greeting("BSU GAME");
-  greeting.show();
-  greeting.resize(300, 100);
+  QListWidget list;
+  TestEcs(&list);
+  list.show();
+  list.resize(300, 100);
   return QApplication::exec();
 }
