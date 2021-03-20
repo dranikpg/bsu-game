1c2d20caf95bf36ec7b2260fc45b377032b6135f
shauneu <shauneu.nm@gmail.com>
2021-03-20 20:28:40 +0300

    Cpplint fix

--- src/components/movement_animation_sync_component.cpp
+++ src/components/movement_animation_sync_component.cpp
@@ -4,8 +4,9 @@
 
 namespace game {
 
-MovementAnimationSyncComponent::MovementAnimationSyncComponent(std::multimap<constants::AnimationType,
-                                               std::shared_ptr<resource::Animation>> animations)
+MovementAnimationSyncComponent::MovementAnimationSyncComponent(
+    std::multimap<constants::AnimationType,
+    std::shared_ptr<resource::Animation>> animations)
      : animations(std::move(animations)),
        current_animation(animations.find(AnimationType::kDefault)->second),
        next_animation(current_animation), current_animation_type(AnimationType::kDefault),
