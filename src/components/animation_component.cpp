3bbbe8b5a4cbd8a281470222f4c7a461c2e62cda
shauneu <shauneu.nm@gmail.com>
2021-04-01 18:26:55 +0300

    Naming fixes

--- src/components/animation_component.cpp
+++ src/components/animation_component.cpp
@@ -4,17 +4,17 @@
 
 namespace game {
 
-AnimationComponent::AnimationComponent(std::shared_ptr<resource::Animation> animation_resource) {
-  SetAnimationResource(std::move(animation_resource));
+AnimationComponent::AnimationComponent(std::shared_ptr<resource::Animation> animation_resource_ptr) {
+  SetAnimationResource(std::move(animation_resource_ptr));
 }
 
 void AnimationComponent::SetAnimationResource(
-    std::shared_ptr<resource::Animation> animation_resource) {
-  animation_resource_ = std::move(animation_resource);
+    std::shared_ptr<resource::Animation> animation_resource_ptr) {
+  animation_resource = std::move(animation_resource_ptr);
   frame_index = 0;
   frame_time = 0;
-  frame_length = animation_resource_->GetFrameLength(frame_index);
-  total_frames = animation_resource_->GetFrameCount();
+  frame_length = animation_resource->GetFrameLength(frame_index);
+  total_frames = animation_resource->GetFrameCount();
   repetitions = 0;
   animation_is_ending = false;
 }
