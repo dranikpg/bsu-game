0fe50744b521f13991b81eabcdd7787a8ca6f564
dranikpg <vladislav.oleshko@gmail.com>
2021-04-14 20:25:28 +0300

    Sort sprites by Y coord before rendering

--- src/systems/rendering_system.cpp
+++ src/systems/rendering_system.cpp
@@ -2,6 +2,8 @@
 
 #include <QPainter>
 
+#include <algorithm>
+
 #include "../components/position_component.h"
 #include "../components/sprite_component.h"
 #include "../components/camera_component.h"
@@ -11,14 +13,16 @@ using constants::SpriteLayer;
 
 namespace game {
 
-RenderingSystem::RenderingSystem(PainterContext* painter_context, WindowContext* window_context)
+RenderingSystem::RenderingSystem(PainterContext* painter_context,
+                                 WindowContext* window_context)
     : painter_context_(painter_context), window_context_(window_context) {}
 
 void RenderingSystem::Run(World* world) {
   QPainter& painter = painter_context_->GetPainter();
   painter.save();
 
-  ecs::Entity* camera_entity = world->ScanEntities<PositionComponent, CameraComponent>().Peek();
+  ecs::Entity* camera_entity =
+      world->ScanEntities<PositionComponent, CameraComponent>().Peek();
   if (camera_entity) {
     auto& camera_pos = camera_entity->GetComponent<PositionComponent>();
     painter.translate(
@@ -26,17 +30,30 @@ void RenderingSystem::Run(World* world) {
         -camera_pos.position.y() + window_context_->GetSize().height() / 2);
   }
 
-  for (int layer_id = 0; SpriteLayer::_LAST != static_cast<SpriteLayer>(layer_id); layer_id++) {
+  std::vector<ecs::Entity*> current_layer;
+
+  for (int layer_id = 0;
+       SpriteLayer::_LAST != static_cast<SpriteLayer>(layer_id); layer_id++) {
     auto layer = static_cast<SpriteLayer>(layer_id);
-    for (auto[sprite, position] : world->Scan<SpriteComponent, PositionComponent>()) {
-      if (sprite.layer != layer) {
-        continue;
+    for (auto& entity : world->ScanEntities<SpriteComponent,
+                                            PositionComponent>()) {
+      if (entity.GetComponent<SpriteComponent>().layer == layer) {
+        current_layer.push_back(&entity);
       }
+    }
+    std::sort(current_layer.begin(), current_layer.end(),
+              [](const ecs::Entity* l, const ecs::Entity* r) {
+                return l->GetComponent<PositionComponent>().position.y() <
+                  r->GetComponent<PositionComponent>().position.y();
+              });
+    for (auto& entity: current_layer) {
+      auto [sprite, position] = entity->Unpack<SpriteComponent, PositionComponent>();
       QRect bounds = sprite.target_area;
       bounds.moveCenter(position.position);
       bounds.translate(sprite.target_area.topLeft());
       painter.drawPixmap(bounds, sprite.pixmap, sprite.source_area);
     }
+    current_layer.clear();
   }
 
   painter.restore();
